c69e33c52a92b1e602e6b67daeee216a2f6f1098
Saar <142357179+SaarY76@users.noreply.github.com>
2023-11-23 18:26:30 +0200

    new fm website

---
+++ fm_manipulations/first_page.js
@@ -0,0 +1,184 @@
+const errorMessageElement = document.getElementById("error_message_file");
+
+function readAndCheckFile() {
+  var fileInput = document.getElementById("fileUpload");
+  const file = fileInput.files[0];
+  const errorMessageElement = document.getElementById("error_message_file");
+  // Check if the file size exceeds 4.5 MB
+  if (file.size > 4.5 * 1024 * 1024) {
+    // 4.5 MB in bytes
+    errorMessageElement.textContent =
+      "The file is above 4.5MB, load an HTML file with Maximum of 4534 Players and 4.5MB";
+    errorMessageElement.style.color = "red";
+    return;
+  }
+
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(e.target.result, "text/html");
+
+    // checking if there is a table element in the HTML file
+    const table = doc.querySelector("table");
+    if (!table) {
+      errorMessageElement.textContent =
+        "There are no players in the table in the HTML file.";
+      return;
+    }
+
+    const rows = table.querySelectorAll("tr");
+
+    if (rows.length === 1) {
+      errorMessageElement.textContent = "There is only one row in the table.";
+      return;
+    }
+
+    // selecting all of the th elements in the first tr
+    const firstRowHeader = table.querySelectorAll("tr:first-child th");
+    const requiredColumnNames = [
+      "Rec",
+      "UID",
+      "Inf",
+      "Name",
+      "Position",
+      "Preferred Foot",
+      "Left Foot",
+      "Right Foot",
+      "Nat",
+      "Club",
+      "Caps",
+      "Height",
+      "Weight",
+      "Age",
+      "Transfer Value",
+      "Wage",
+      "Min Fee Rls",
+      "Cor",
+      "Cro",
+      "Dri",
+      "Fin",
+      "Fir",
+      "Fre",
+      "Hea",
+      "Lon",
+      "L Th",
+      "Mar",
+      "Pas",
+      "Pen",
+      "Tck",
+      "Tec",
+      "Agg",
+      "Ant",
+      "Bra",
+      "Cmp",
+      "Cnt",
+      "Dec",
+      "Det",
+      "Fla",
+      "Ldr",
+      "OtB",
+      "Pos",
+      "Tea",
+      "Vis",
+      "Wor",
+      "Acc",
+      "Agi",
+      "Bal",
+      "Jum",
+      "Nat",
+      "Pac",
+      "Sta",
+      "Str",
+      "Aer",
+      "Cmd",
+      "Com",
+      "Ecc",
+      "Han",
+      "Kic",
+      "1v1",
+      "Pun",
+      "Ref",
+      "TRO",
+      "Thr",
+    ];
+
+    let foundColumnNames = Array.from(firstRowHeader).map((th) =>
+      th.innerText.trim()
+    );
+
+    // Checking if each required column name is present in the first row of the table
+    const hasRequiredColumns = requiredColumnNames.every((colName) =>
+      foundColumnNames.includes(colName.trim())
+    );
+
+    if (!hasRequiredColumns) {
+      errorMessageElement.textContent =
+        "The user didn't use the specific player search view.";
+      return;
+    }
+
+    errorMessageElement.textContent = "All is Good"; // Clear error message if everything is fine
+    errorMessageElement.style.color = "green";
+
+    sessionStorage.setItem("uploadedTableData", table.outerHTML);
+
+    // Redirect to second_page.html
+    window.location.href = "./second_page.html";
+  };
+
+  reader.onerror = function () {
+    errorMessageElement.textContent = "Error reading file";
+  };
+
+  reader.readAsText(file);
+}
+
+function showResultsAfterUserSelectedFile() {
+  var fileInput = document.getElementById("fileUpload");
+  var fileChosen = document.getElementById("fileChosen");
+
+  if (fileInput.files.length > 0) {
+    fileChosen.textContent = fileInput.files[0].name;
+    readAndCheckFile();
+  } else {
+    fileChosen.textContent = "לא נבחר קובץ";
+  }
+}
+
+window.addEventListener("pageshow", function () {
+  var fileInput = document.getElementById("fileUpload");
+  var errorMessageElement = document.getElementById("error_message_file");
+
+  // Clear the file input and any error messages
+  fileInput.value = "";
+  errorMessageElement.textContent = "";
+
+  // Optionally, clear session storage
+  sessionStorage.removeItem("uploadedTableData");
+});
+
+/**
+ * this function is for downloading the Football Manager View for players search, so the user will able to use this website
+ */
+function downloadViewFile() {
+  // URL to the .fmf file
+  const fileUrl = "./FM All Attributes.fmf";
+
+  // Creating an anchor element and trigger download
+  const anchor = document.createElement("a");
+  anchor.href = fileUrl;
+  anchor.download = "FM All Attributes.fmf";
+  anchor.click();
+
+  // Optional: remove the anchor element after initiating download
+  anchor.remove();
+}
+
+function moveToUploadHtmlPageMenual() {
+  window.location.href = "./menual_upload_html.html";
+}
+
+function moveToDownloadViewPageMenual() {
+  window.location.href = "./menual_download_view.html";
+}
---
+++ fm_manipulations/second_page - Copy.js
@@ -0,0 +1,282 @@
+let page_table = null;
+function createFilteredTable() {
+  const tableData = getUploadedTableData(); // Retrieve the table data as HTML string
+  if (!tableData) {
+    console.error("No table data available.");
+    return;
+  }
+
+  // Define the required columns
+  const requiredColumns = [
+    "Rec",
+    "UID",
+    "Inf",
+    "Name",
+    "Position",
+    "Preferred Foot",
+    "Left Foot",
+    "Right Foot",
+    "Nat",
+    "Club",
+    "Caps",
+    "Height",
+    "Weight",
+    "Age",
+    "Transfer Value",
+    "Wage",
+    "Min Fee Rls",
+  ];
+
+  // Create a new table element
+  const newTable = document.createElement("table");
+  newTable.setAttribute("border", "1");
+
+  // Add table header
+  const thead = newTable.createTHead();
+  const headerRow = thead.insertRow();
+  requiredColumns.forEach((colName) => {
+    const th = document.createElement("th");
+    th.textContent = colName;
+    headerRow.appendChild(th);
+  });
+
+  // Add table body
+  const tbody = newTable.createTBody();
+  const parser = new DOMParser();
+  const originalTable = parser
+    .parseFromString(tableData, "text/html")
+    .querySelector("table");
+
+  // Map to store the first occurrence of each column
+  const columnIndexMap = new Map();
+
+  // Extract column indices from the first row
+  const headerCells = originalTable.rows[0].cells;
+  for (let i = 0; i < headerCells.length; i++) {
+    const colName = headerCells[i].textContent;
+    if (requiredColumns.includes(colName) && !columnIndexMap.has(colName)) {
+      columnIndexMap.set(colName, i);
+    }
+  }
+
+  // Iterate over each row in the original table
+  for (let i = 1; i < originalTable.rows.length; i++) {
+    const row = originalTable.rows[i];
+    const newRow = tbody.insertRow();
+
+    requiredColumns.forEach((colName) => {
+      const cellIndex = columnIndexMap.get(colName);
+      if (cellIndex !== undefined) {
+        const newCell = row.cells[cellIndex].cloneNode(true);
+        newRow.appendChild(newCell);
+      }
+    });
+  }
+
+  // Append the new table to the body or a specific element
+  const form_table_container = document.getElementById("form_table_container");
+  form_table_container.appendChild(newTable); // or document.getElementById("yourElementId").appendChild(newTable);
+  page_table = newTable;
+}
+
+//////////////////////////////////////////// check
+document.getElementById("roleSelectionForm").onsubmit = function (event) {
+  event.preventDefault();
+
+  // Get all checked checkboxes
+  const checkedRoles = Array.from(
+    document.querySelectorAll(
+      '#roleSelectionForm input[type="checkbox"]:checked'
+    )
+  ).map((el) => el.value);
+
+  // Process the table based on the checked roles
+  processTableForSelectedRoles(checkedRoles, page_table);
+};
+
+function processTableForSelectedRoles(roles, newTable) {
+  const tableData = getUploadedTableData();
+  if (!tableData) {
+    console.error("No table data available.");
+    return;
+  }
+
+  const parser = new DOMParser();
+  const originalTable = parser
+    .parseFromString(tableData, "text/html")
+    .querySelector("table");
+  const columnIndexMap = getColumnIndexMap(originalTable);
+
+  // Remove previously added rating columns if any
+  removeRatingColumns(newTable);
+
+  // Add new column headers and data for each selected role
+  addNewColumnsWithRatings(newTable, roles, originalTable, columnIndexMap);
+}
+
+function addNewColumnsWithRatings(
+  newTable,
+  roles,
+  originalTable,
+  columnIndexMap
+) {
+  const headerRow = newTable.rows[0];
+
+  // Add new column headers for each role
+  roles.forEach((role, index) => {
+    const th = document.createElement("th");
+    th.textContent = role + " Rating";
+    const sortArrow = document.createElement("span");
+    sortArrow.innerHTML = "&#9660;"; // Downward arrow for descending order
+    sortArrow.style.cursor = "pointer";
+    th.appendChild(sortArrow);
+
+    // Initialize sorting direction as descending
+    let ascending = false;
+    th.addEventListener("click", () => {
+      ascending = !ascending; // Toggle the direction
+      updateSortArrow(sortArrow, ascending);
+      sortTableByColumn(
+        newTable,
+        headerRow.cells.length - roles.length + index,
+        !ascending
+      );
+    });
+
+    headerRow.appendChild(th);
+  });
+
+  // Add rating data for each player for each role
+  for (let i = 1; i < newTable.rows.length; i++) {
+    const newRow = newTable.rows[i];
+    const uid = newRow.cells[columnIndexMap.get("UID")].textContent; // Get UID from the new row
+
+    // Find the corresponding row in the original table using UID
+    const originalRow = findRowByUID(originalTable, uid, columnIndexMap);
+
+    roles.forEach((role) => {
+      const roleAttributes = all_roles[role];
+      let rating = "N/A";
+      if (roleAttributes && originalRow) {
+        const playerData = extractPlayerData(originalRow, columnIndexMap);
+        rating = calculateAverageRating(playerData, roleAttributes);
+      }
+      appendRatingToRow(newRow, rating);
+    });
+  }
+}
+
+function findRowByUID(table, uid, columnIndexMap) {
+  for (let i = 1; i < table.rows.length; i++) {
+    const row = table.rows[i];
+    if (row.cells[columnIndexMap.get("UID")].textContent === uid) {
+      return row;
+    }
+  }
+  return null; // Return null if no matching row is found
+}
+
+function removeRatingColumns(table) {
+  // Assuming the original number of columns (without ratings) is known
+  const originalColumnCount = 16; // Update this with the number of your original columns
+
+  let headerRow = table.rows[0];
+  while (headerRow.cells.length > originalColumnCount) {
+    headerRow.deleteCell(-1); // Remove the last cell in the header row
+  }
+
+  // Do the same for each row in the table
+  for (let i = 1; i < table.rows.length; i++) {
+    while (table.rows[i].cells.length > originalColumnCount) {
+      table.rows[i].deleteCell(-1); // Remove the last cell in each row
+    }
+  }
+}
+
+function getColumnIndexMap(table) {
+  const headerCells = table.rows[0].cells;
+  const columnIndexMap = new Map();
+  for (let i = 0; i < headerCells.length; i++) {
+    columnIndexMap.set(headerCells[i].textContent, i);
+  }
+  return columnIndexMap;
+}
+
+function extractPlayerData(row, columnIndexMap) {
+  let playerData = {};
+  columnIndexMap.forEach((index, columnName) => {
+    playerData[columnName] = row.cells[index].textContent;
+  });
+  return playerData;
+}
+
+function calculateAverageRating(playerData, roleAttributes) {
+  let totalScore = 0;
+  let totalLevel = 0;
+
+  roleAttributes.forEach((attr) => {
+    let attributeValue = parseInt(playerData[attr.atr], 10);
+    if (!isNaN(attributeValue)) {
+      totalScore += attributeValue * attr.lvl;
+      totalLevel += attr.lvl;
+    }
+  });
+
+  return totalLevel > 0 ? (totalScore / totalLevel).toFixed(2) : "N/A";
+}
+
+function appendRatingToRow(row, rating) {
+  const ratingCell = row.insertCell(-1);
+  ratingCell.textContent = rating;
+}
+
+function addSortableHeaders(newTable, roles) {
+  const headerRow = newTable.rows[0];
+  roles.forEach((role, index) => {
+    const th = document.createElement("th");
+    th.textContent = role + " Rating ";
+    const sortArrow = document.createElement("span");
+    sortArrow.style.cursor = "pointer";
+    th.appendChild(sortArrow);
+
+    // Initialize sorting direction (start with descending)
+    let ascending = false;
+    updateSortArrow(sortArrow, ascending);
+    th.addEventListener("click", () => {
+      ascending = !ascending; // Toggle the direction
+      updateSortArrow(sortArrow, ascending);
+      sortTableByColumn(
+        newTable,
+        headerRow.cells.length - roles.length + index,
+        ascending
+      );
+    });
+
+    headerRow.appendChild(th);
+  });
+}
+
+function updateSortArrow(arrowElement, ascending) {
+  arrowElement.innerHTML = ascending ? "&#9650;" : "&#9660;"; // Up arrow for ascending, down arrow for descending
+}
+
+function sortTableByColumn(table, columnIndex, ascending = true) {
+  const dirModifier = ascending ? 1 : -1;
+  // Select all rows containing player data
+  const rows = Array.from(table.querySelectorAll("tr")).slice(1); // Skip the header row
+
+  // Sort the rows based on the content of cells in the specified column
+  rows.sort((a, b) => {
+    const aColText = a.cells[columnIndex].textContent.trim();
+    const bColText = b.cells[columnIndex].textContent.trim();
+    return (
+      aColText.localeCompare(bColText, undefined, { numeric: true }) *
+      dirModifier
+    );
+  });
+
+  // Append the sorted rows back to the table
+  rows.forEach((row) => table.appendChild(row));
+}
+
+createFilteredTable();
